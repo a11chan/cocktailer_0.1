dce1f7ec386ff85033b0257277b4012038f71231
baeksia <baeksiah@gmail.com>
2021-09-26 22:38:13 +0900

    final

---
+++ js/sub.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $("#trigger").click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass("active");
+    $("#mainmenu").toggleClass("active");
+  });
+});
